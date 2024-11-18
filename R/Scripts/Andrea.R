0ee1a67d15f74750baf24326e7180d0954a76c0e
Andrea <Andrea.Wu@monash.edu>
2024-11-18 16:05:45 +1100

    add a new CEPlane

--- R/Scripts/Andrea.R
+++ R/Scripts/Andrea.R
@@ -3,6 +3,7 @@ ggplot2::ggplot(read.csv("data/results.csv"), ggplot2::aes(IncrementalQALYs, Inc
   ggplot2::geom_point() +
   ggplot2::scale_color_manual(values = c('#E69F00'))  +
   ggplot2::xlab("Incremental QALYs") + ggplot2::ylab("Incremental costs") +
-  ggplot2::geom_abline(slope = 50000, linetype = "dashed") +
+  ggplot2::geom_abline(slope = 30000, linetype = "dashed") +
   ggplot2::geom_hline(yintercept = 0) +
   ggplot2::geom_vline(xintercept = 0)
+
